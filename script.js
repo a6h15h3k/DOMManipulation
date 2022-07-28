5c01d2a0ffc956a7e9bc6c0a6d95792447f08130
a6hi <saraffabhishek@outlook.com>
2022-07-27 21:57:09 -0400

    Step 1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let container = document.querySelector('#container');
+
+/* a <p> with red text that says “Hey I’m red! */
+let div = document.createElement('div');
+div.style.color = 'red';
+div.textContent = 'Hey, I\'m red!';
+container.appendChild(div);
