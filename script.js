85111473769ba75489ae5dda262fab8f0e7be34f
a6hi <saraffabhishek@outlook.com>
2022-07-27 21:57:45 -0400

    Step 2

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let div = document.createElement('div');
 div.style.color = 'red';
 div.textContent = 'Hey, I\'m red!';
 container.appendChild(div);
+
+/* an <h3> with blue text that says “I’m a blue h3!”! */
+div = document.createElement('h3');
+div.style.color = 'blue';
+div.textContent = 'I\'m a blue h3!';
+container.appendChild(div);
